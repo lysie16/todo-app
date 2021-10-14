309d44a034c0497d79ddbd11d7d068a4351018bf
lysie16 <lysie16@hotmail.ca>
2021-10-14 19:38:10 -0400

    Added form fields and styles

--- src/components/pages/all-todos/index.jsx
+++ src/components/pages/all-todos/index.jsx
@@ -1,23 +1,16 @@
 import "./styles.css";
-import { Todo } from "./components/todo/index";
+import { Todo } from "../../todo/index";
 
 export const AllTodosPage = () => {
-    return (
-        <div>
-            <Todo
-                text="Walk my dog" date="10/10/2021" color = "blue"
-                />
-            
-            <Todo
-            text="Buy laundry detergent"
-            date="10/10/2021" color="blue"
-            />
-            
-            <Todo
-            text="Go Workout :(" date="10/10/2021" color="blue"
-            />
-            
+    const todos= [
+        {text:"Walk my dog", date:"10/10/2021", color:"blue", isComplete: false},
+        {text:"Go Workout", date:"10/10/2021", color:"red", isComplete: false},
+        {text:"Do assignment", date:"10/10/2021", color:"red", isComplete: true},
+    ];
 
+    return (
+        <div className="todos-container">
+            {todos.map ((todo) => <Todo text={todo.text} date={todo.date} color={todo.color} isComplete={todo.isComplete} />) }
          </div>
     )
 }
